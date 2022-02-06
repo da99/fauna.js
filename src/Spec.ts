f53769952ee73b815a89b036a14719c412fd6148
da99 <1551848+da99@users.noreply.github.com>
2022-02-06 16:49:10 -0600

    Finished: Spec.ts w/o Deno.test

--- src/Spec.ts
+++ src/Spec.ts
@@ -1,11 +1,19 @@
 
+import caller from 'https://raw.githubusercontent.com/apiel/caller/master/caller.ts';
 import { Text_File } from "../src/Text_File.ts";
 import { colorize } from "../src/CLI.ts";
+import { assertEquals as EQUALS } from "https://deno.land/std/testing/asserts.ts";
 import { bold as BOLD, blue as BLUE, green as GREEN, red as RED  } from "https://deno.land/std/fmt/colors.ts";
 
 // # =============================================================================
 type Asyn_Function = () => Promise<void>;
 type Void_Function = () => void;
+interface Print_Stack_Record  {
+  filename?: string;
+  describe? : string;
+  it? : string;
+  async_f? : Asyn_Function;
+}
 
 
 // # =============================================================================
@@ -18,43 +26,23 @@ function is_async_function(x: any) {
 } // function
 
 // # =============================================================================
-let current_desc                   = "Unknown";
-let current_it                     = false;
-let has_printed_filename           = false;
-let last_fail_title: null | string = null;
-let the_file_name: null | string = null;
+const LAST_FAIL_FILE = "tmp/spec/last.fail";
+const PRINT_STACK: Array<Print_Stack_Record> = [];
 
-const LAST_FAIL_FILE                             = "tmp/spec/last.fail";
-const PRINT_STACK: Array<Record<string, string>> = [];
-
-try{
-  last_fail_title = Deno.readTextFileSync(LAST_FAIL_FILE);
-} catch(e) {
-  // ignore
-}
+function get_content(s: string) {
+  try{
+    return Deno.readTextFileSync(s);
+  } catch(e) {
+    return null;
+  }
+} // function
 
 export function filename(f: string) {
-  if (f.indexOf(":")) {
-    the_file_name = (new URL(f)).pathname;
-  } else {
-    the_file_name = f;
-  }
-} // function
-
-function it_last_fail(raw_title: string) {
-  PRINT_STACK[0]
-  return `${current_desc} ${raw_title}`;
-} // function
-
-function print_filename() {
-  if (has_printed_filename) {
-    return false;
+  if (f.indexOf(":") > 0) {
+    return (new URL(f)).pathname;
   }
 
-  const pathname = (new URL(import.meta.url)).pathname;
-  prompt(`\nFile: ${BOLD(the_file_name || "unknown")}\n`);
-  has_printed_filename = true;
-  return true;
+  return f;
 } // function
 
 function prompt(raw_text: string) {
@@ -64,60 +52,74 @@ function prompt(raw_text: string) {
   );
 } // function
 
+export async function finish() {
+  let last_filename       = null;
+  let last_desc           = null;
+  const LAST_FAIL_VERSION = get_content(LAST_FAIL_FILE);
+
+  for (const x of PRINT_STACK) {
+    if (x.describe) {
+      last_desc = x.describe;
+      prompt(`${BOLD(x.describe as string)}\n`);
+      continue;
+    }
+
+    if (x.filename) {
+      last_filename = x.filename;
+      prompt(`\nFile: ${BOLD(x.filename)}\n`);
+      continue;
+    }
+
+    if (x.it && x.async_f) {
+      const res = Deno.resources();
+      const version = `${last_filename} ${last_desc} ${x.it}`;
+
+      if (LAST_FAIL_VERSION && version !== LAST_FAIL_VERSION) {
+        continue;
+      }
+
+      prompt(`  ${x.it as string} `);
+
+      try {
+        await x.async_f();
+        prompt(GREEN(`${CHECK_MARK}\n`));
+        EQUALS(res, Deno.resources());
+        if (LAST_FAIL_VERSION === version) {
+          Deno.remove(LAST_FAIL_FILE);
+          break;
+        }
+      } catch(e) {
+        prompt(BOLD(RED(`${X_MARK}\n`)));
+        if (LAST_FAIL_VERSION !== version) {
+          Deno.writeTextFileSync(LAST_FAIL_FILE, version);
+        }
+        throw e;
+      }
+    } // if/else
+  } // for
+} // function
+
+let module_caller: undefined | string = import.meta.url;
+let unknown_caller = 0;
+
 export function describe(title: string) {
-  current_desc = title;
+  const current_caller = filename(caller() || `[UNKNOWN FILE ${++unknown_caller}`);
+  if (current_caller !== module_caller) {
+    PRINT_STACK.push({filename: current_caller});
+    module_caller = current_caller
+  }
+  PRINT_STACK.push({describe: title});
 } // function
 
 export function it(raw_title: string, raw_f: Void_Function | Asyn_Function) {
-  const desc_title   = current_desc;
-  const full_title   = `${desc_title} ${raw_title}`;
-  const version      = `${the_file_name} ${full_title}`;
-  const is_last_fail = (last_fail_title === version)
-  const first_it     = !current_it;
-  const do_print_filename = !has_printed_filename;
-  current_it = true;
-
   const f: null | (() => Promise<void>) = (raw_f.constructor.name === "Async Function") ?
     (raw_f as Asyn_Function) :
     (function () { return Promise.resolve(raw_f()); });
 
-  const wrapper_f = async function () {
-    if (do_print_filename)
-      print_filename();
-    if (first_it)
-      prompt(`${desc_title}\n`);
-    prompt(`  ${raw_title}`);
-
-    if (last_fail_title && !is_last_fail) {
-      prompt(` ???\n`);
-      return;
-    }
-
-    let has_pass = false;
-
-    try {
-      await f();
-      prompt(colorize(` ${CHECK_MARK}\n`, "GREEN"));
-      has_pass = true;
-      if (is_last_fail) {
-        await Deno.remove(LAST_FAIL_FILE);
-      }
-    } catch (e) {
-      prompt(colorize(` ${X_MARK}\n`, "RED", "BOLD"));
-      prompt(e.message);
-      has_pass = false;
-      await Deno.writeTextFile(LAST_FAIL_FILE, version);
-      throw e;
-    }
-  }; // async
-
-  if (!last_fail_title || is_last_fail) {
-    prompt(`${Deno.inspect(last_fail_title)} ${Deno.inspect(is_last_fail)}`);
-    return Deno.test({
-      name: raw_title,
-        fn: wrapper_f
-    });
-  } // if
+  PRINT_STACK.push({
+    "it": raw_title,
+    "async_f": f
+  });
 } // function
 
 

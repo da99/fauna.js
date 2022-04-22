f858943f5b652308212e2b38a2aadfde132f99e3
da99 <1551848+da99@users.noreply.github.com>
2022-04-22 11:23:01 -0500

    Finished: bunny.ts

--- bin/bunny.ts
+++ bin/bunny.ts
@@ -1,10 +1,10 @@
-#!/usr/bin/env -S deno run --allow-run=fd,find --allow-net=storage.bunnycdn.com --allow-env=BUNNY_URL,BUNNY_KEY --allow-read=./ --allow-write=./
+#!/usr/bin/env -S deno run --allow-run=fd,find,git --allow-net=storage.bunnycdn.com --allow-env=BUNNY_URL,BUNNY_KEY --allow-read=./ --allow-write=./
 
 // import * as path from "https://deno.land/std/path/mod.ts";
 import {meta_url, match, values, not_found} from "../src/CLI.ts";
 import { green, red, yellow, bold } from "https://deno.land/std/fmt/colors.ts";
-import {content_type, human_bytes, MB, sort_by_key} from "../src/Function.ts";
-import {fd, columns} from "../src/Shell.ts";
+import {content_type, human_bytes, MB, sort_by_key, count} from "../src/Function.ts";
+import {fd, columns, shell} from "../src/Shell.ts";
 import { readableStreamFromReader } from "https://deno.land/std/streams/conversion.ts";
 import * as path from "https://deno.land/std/path/mod.ts";
 import {
@@ -53,18 +53,28 @@ export interface Bunny_File {
   "Checksum":        string;
 };
 
+export interface Delete_Status extends Bunny_File {
+  ok: boolean;
+  json: Bunny_Response;
+};
+
 // =============================================================================
 // IF MATCH:
 // =============================================================================
 
-if (match("ls local files verbose")) {
+if (match(`project name`)) {
+  const name = await project_name();
+  console.log(name);
+} // if
+
+if (match("files . -v")) {
   const files = await local_files();
   for (const f of files) {
     verbose_log_file(f.local_path, f);
   } // for
 } // if
 
-if (match("ls local files", "Be sure to 'cd' into the Public directory you want to upload.")) {
+if (match("files .", "Be sure to 'cd' into the Public directory you want to upload.")) {
   const files = await local_files();
   for (const f of files) {
     if (f.bytes > MB)
@@ -74,9 +84,9 @@ if (match("ls local files", "Be sure to 'cd' into the Public directory you want 
   } // for
 } // if
 
-if (match(`ls remote files in <remote_dirname> verbose`)) {
-  const [dirname] = values() as string[];
-  const files     = await remote_files(dirname);
+if (match(`files remote -v`)) {
+  const dirname = await project_name();
+  const files   = await remote_files(dirname);
 
   if (files.length === 0)
     console.error(`${yellow('===')} No files found for: ${bold(dirname)}`);
@@ -86,9 +96,9 @@ if (match(`ls remote files in <remote_dirname> verbose`)) {
   } // for
 } // if
 
-if (match(`ls remote files in <remote_dirname>`)) {
-  const [dirname] = values() as string[];
-  const files     = await remote_files(dirname);
+if (match(`files remote`)) {
+  const dirname = await project_name();
+  const files   = await remote_files(dirname);
 
   if (files.length === 0)
     console.error(`${yellow('===')} No files found for: ${bold(dirname)}`);
@@ -101,22 +111,23 @@ if (match(`ls remote files in <remote_dirname>`)) {
   } // for
 } // if
 
-if (match(`ls files to upload to <remote_dirname> verbose`)) {
-  const [dirname] = values() as string[];
+if (match(`files to upload -v`)) {
+  const dirname = await project_name();
+
   (await uploadable_local_files(dirname)).forEach(lf => {
     verbose_log_local_file(lf);
   })
 } // if
 
-if (match(`ls files to upload to <remote_dirname>`)) {
-  const [dirname] = values() as string[];
+if (match(`files to upload`)) {
+  const dirname = await project_name();
   for (const lf of (await uploadable_local_files(dirname))) {
     console.log(`${bold(lf.local_path)} ${lf.remote_path}`);
   } // for
 } // if
 
-if (match(`upload files to <remote_dirname>`)) {
-  const [dirname]                    = values() as string[];
+if (match(`upload files`)) {
+  const dirname = await project_name();
   const url   = bunny_url(dirname);
   console.error(`=== URL: ${yellow(url)}`);
   const results = await upload_files(dirname);
@@ -133,21 +144,8 @@ if (match(`upload files to <remote_dirname>`)) {
   } // for
 } // if
 
-if (match('ls old remote files in <dirname>')) {
-  const [dirname] = values() as string[];
-  const old_bunnys = await old_files(dirname);
-  for (const bf of old_bunnys) {
-    if (bf.Length > MB)
-      console.log(`${bf.ObjectName} ${bold(human_bytes(bf.Length))}`);
-    else
-      console.log(`${bf.ObjectName}`);
-  } // for
-  if (old_bunnys.length === 0)
-    console.error(`=== ${yellow(`No old remote files`)} in ${bold(dirname)}.`);
-} // if
-
-if (match('ls old remote files in <dirname> long')) {
-  const [dirname] = values() as string[];
+if (match('old remote files -v')) {
+  const dirname = await project_name();
   const old_bunnys = await old_files(dirname);
   for (const bf of old_bunnys) {
     console.log(`${yellow(bf.ObjectName)}`)
@@ -164,6 +162,32 @@ if (match('ls old remote files in <dirname> long')) {
     console.error(`=== ${yellow(`No old remote files`)} in ${bold(dirname)}.`);
 } // if
 
+if (match('old remote files')) {
+  const dirname = await project_name();
+  const old_bunnys = await old_files(dirname);
+  for (const bf of old_bunnys) {
+    if (bf.Length > MB)
+      console.log(`${bf.ObjectName} ${bold(human_bytes(bf.Length))}`);
+    else
+      console.log(`${bf.ObjectName}`);
+  } // for
+  if (old_bunnys.length === 0)
+    console.error(`=== ${yellow(`No old remote files`)} in ${bold(dirname)}.`);
+} // if
+
+if (match('delete old remote files')) {
+  const dirname = await project_name();
+  const old = await delete_old_files(dirname);
+  if (old.length === 0)
+    console.error(`=== ${yellow("Nothing to delete.")}`);
+  for (const dstat of old) {
+    if (dstat.ok)
+      console.log(`${green("✓")} ${bold(dstat.ObjectName)} ${Deno.inspect(dstat.json, {colors: true})}`)
+    else
+      console.log(`${red("✗")} ${yellow(dstat.ObjectName)} ${Deno.inspect(dstat.json, {colors: true})}`)
+  } // for
+} // if
+
 // =============================================================================
 not_found();
 // =============================================================================
@@ -171,6 +195,14 @@ not_found();
 // =============================================================================
 // Functions:
 // =============================================================================
+
+export async function project_name() {
+  const name = (await shell("git", "remote get-url origin"))
+  .raw.slice(0,1).join('').trim().replace(/\.git$/, '').split('/').pop();
+  if (typeof name === "string" && name.length > 0)
+    return name;
+  throw new Error(`No project name could be found.`);
+} // export async function
 
 function bunny_url(dirname: string) {
   return `${env_or_throw("BUNNY_URL")}/${dirname}/`;;
@@ -315,3 +347,38 @@ export function verbose_log_file(title: string, f: Bunny_File | Local_File) {
     } // for k,v
     console.log("================================================================================");
 } // export function
+
+export async function delete_old_files(dirname: string): Promise<Delete_Status[]> {
+  const old                    = await old_files(dirname);
+  const stats: Delete_Status[] = [];
+  const PROJECT_URL            = bunny_url(dirname);
+  const BUNNY_KEY              = env_or_throw("BUNNY_KEY");
+
+  if (old.length === 0)
+    return stats;
+
+  const fetches = old.map(bf => {
+    return fetch(
+      new Request(
+        path.join(PROJECT_URL, bf.ObjectName),
+        {
+          method: 'DELETE',
+          headers: new Headers({
+            AccessKey: BUNNY_KEY,
+          })
+        }
+      )
+    ); // fetch
+  });
+
+  const resps = await Promise.all(fetches);
+  const json = await Promise.all(resps.map(x => x.json()));
+
+  for (const i of count(old.length)) {
+    stats.push(
+      Object.assign({}, old[i], {ok: resps[i].ok, json: json[i]})
+    );
+  } // for
+
+  return stats;
+} // export async function
